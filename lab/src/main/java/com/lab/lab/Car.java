2421909f64335bf085c2be37dea3f1420ed31a73
ailkovjr <ailkovjr@gmail.com>
2019-01-21 00:41:23 +0100

    Application for the lab
    Signed-off-by: ailkovjr <ailkovjr@gmail.com>

---
+++ lab/src/main/java/com/lab/lab/Car.java
@@ -0,0 +1,54 @@
+package com.lab.lab;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Car {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+
+	private String make;
+	
+	private String model;
+	
+	private int yearMade;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public int getYearMade() {
+		return yearMade;
+	}
+
+	public void setYearMade(int yearMade) {
+		this.yearMade = yearMade;
+	}
+
+}
---
+++ lab/src/main/java/com/lab/lab/CarRepository.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.lab.lab;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.lab.lab.Car;
+/**
+ * @author ilkovjr
+ *
+ */
+public interface CarRepository extends CrudRepository<Car, Integer> {
+
+}
